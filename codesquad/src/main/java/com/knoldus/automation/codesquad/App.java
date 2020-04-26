5089f5b566f0e3781146cb2108e30e0be43afdd3
mukesh kumar <mukesh.kumar@knoldus.in>
2020-04-26 23:08:30 +0530

    initial commit

---
+++ codesquad/src/main/java/com/knoldus/automation/codesquad/App.java
@@ -0,0 +1,13 @@
+package com.knoldus.automation.codesquad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
